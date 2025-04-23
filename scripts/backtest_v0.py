49912ab3b207f8c0812335c7db52b0d29ded61ea
hugo <hugomiquel498@gmail.com>
2025-04-23 20:59:01 +0200

    Refactor backtest script to use BuyHeikinGreen strategy and enable stochastic parameters

--- scripts/backtest_v0.py
+++ scripts/backtest_v0.py
@@ -1,6 +1,7 @@
 from igtrader.Strategies.BuyTrendFollowing import BuyTrendFollowingBA
 from igtrader.Strategies.SellTrendFollowing import SellTrendFollowingBA
 from igtrader.Strategies.CrossEMA import CrossEMABA
+from igtrader.Strategies.BuyHeikinGreen import BuyHeikinGreenBA
 from igtrader.Strategies.Helpers import load_data
 from igtrader.backtestingpy.backtesting.backtesting import Backtest
 
@@ -10,7 +11,7 @@ from datetime import time
 data = load_data(symbol='NDX', period='1d', interval='20secs')
 
 print(data.head())
-strategy = CrossEMABA
+strategy = BuyHeikinGreenBA
 strategy_kwargs = {
   # base
   'trading_from': time(19, 0),
@@ -21,9 +22,9 @@ strategy_kwargs = {
   # spécifique
   'ema_short_period': 50,
   'ema_long_period': 200,
-#   'stoch_fastk': 10,
-#   'stoch_slowk': 7,
-#   'stoch_slowd': 3,
+  'stoch_fastk': 10,
+  'stoch_slowk': 7,
+  'stoch_slowd': 3,
 }
 bt = Backtest(data, strategy, cash=100000, commission=.00, exclusive_orders=True, strategy_kwargs=strategy_kwargs)
 stats = bt.run()

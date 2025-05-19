ccd88a27f914bf4846cdb61ae2487326370fe2c0
maks7d <maxime.deville@free.fr>
2025-05-19 20:32:10 +0200

    removed talib

--- igtrader/Strategies/BuyTrendFollowing/__init__.py
+++ igtrader/Strategies/BuyTrendFollowing/__init__.py
@@ -3,7 +3,6 @@ from igtrader.backtestingpy.backtesting.backtesting import Strategy as Backtesti
 import logging
 import numpy as np
 from datetime import time
-import talib
 from dataclasses import dataclass
 
 @dataclass

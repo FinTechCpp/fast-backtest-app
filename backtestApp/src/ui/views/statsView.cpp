0f01b5d04a527a72e6c4a77a2aa66263f6b939bf
hugo <hugomiquel498@gmail.com>
2025-07-22 21:58:15 +0200

    feat: update metric formatting to display percentage first for trade statistics

--- backtestApp/src/ui/views/statsView.cpp
+++ backtestApp/src/ui/views/statsView.cpp
@@ -584,26 +584,26 @@ void StatsView::initializeMetricDefinitions() {
         },
         {"TP_trades", "Trades sur take-profit:", "Nombre de trades sur take-profit", "general",
             [](const be::Stats& s) { return MetricStatus::Good; },
-            [](const be::Stats& s) { return QString::number(s.numTPTrades) + " (" + QString::number(s.pctTPTrades, 'f', 2) + "%)"; }
+            [](const be::Stats& s) { return QString::number(s.pctTPTrades) + "% (" + QString::number(s.numTPTrades, 'f', 2) + ")"; }
         },
         {"SL_trades", "Trades sur stop-loss:", "Nombre de trades sur stop-loss", "general",
             [](const be::Stats& s) { 
                 return s.numSLTrades <= s.numTPTrades ? MetricStatus::Neutral : MetricStatus::Bad; 
             },
-            [](const be::Stats& s) { return QString::number(s.numSLTrades) + " (" + QString::number(s.pctSLTrades, 'f', 2) + "%)"; }
+            [](const be::Stats& s) { return QString::number(s.pctSLTrades) + "% (" + QString::number(s.numSLTrades, 'f', 2) + ")"; }
         },
         {"BE_trades", "Trades sur break-even:", "Nombre de trades sur break-even", "general",
             [](const be::Stats& s) { return s.numBETrades <= s.numTPTrades ? MetricStatus::Neutral : MetricStatus::Bad; },
-            [](const be::Stats& s) { return QString::number(s.numBETrades) + " (" + QString::number(s.pctBETrades, 'f', 2) + "%)"; }
+            [](const be::Stats& s) { return QString::number(s.pctBETrades) + "% (" + QString::number(s.numBETrades, 'f', 2) + ")"; }
         },
         // ajouter les metric : numManualTrades et numUnknownTrades
         {"manual_trades", "Trades manuels:", "Nombre de trades manuels", "general",
             [](const be::Stats& s) { return s.numManualTrades > 0 ? MetricStatus::Neutral : MetricStatus::Good; },
-            [](const be::Stats& s) { return QString::number(s.numManualTrades) + " (" + QString::number(s.pctManualTrades, 'f', 2) + "%)"; }
+            [](const be::Stats& s) { return QString::number(s.pctManualTrades) + "% (" + QString::number(s.numManualTrades, 'f', 2) + ")"; }
         },
         {"unknown_trades", "Trades inconnus:", "Nombre de trades avec raison de clôture inconnue", "general",
             [](const be::Stats& s) { return s.numUnknownTrades > 0 ? MetricStatus::Bad : MetricStatus::Good; },
-            [](const be::Stats& s) { return QString::number(s.numUnknownTrades) + " (" + QString::number(s.pctUnknownTrades, 'f', 2) + "%)"; }
+            [](const be::Stats& s) { return QString::number(s.pctUnknownTrades) + " (" + QString::number(s.numUnknownTrades, 'f', 2) + "%)"; }
         },
         {"best_trade", "Meilleur trade:", "Pourcentage de gain du meilleur trade", "general",
             [](const be::Stats& s) { return MetricStatus::Good; },
